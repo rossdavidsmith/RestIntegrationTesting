ca106ad9eb964451c8c764c4e7b84a2e51faecb8
Ross Smith <ross.david.smith@gmail.com>
2018-10-13 16:13:35 +0100

    Add more basic solution infrastructure

---
+++ Example.WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace Example.WebApi
+{
+    public static class WebApiConfig
+    {
+        private const string DefaultRouteName = "DefaultRoute";
+
+        public static void Register(HttpConfiguration configuration)
+        {
+            configuration.Routes.MapHttpRoute(
+                name: DefaultRouteName,
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+
+            configuration.EnableSystemDiagnosticsTracing();
+        }
+    }
+}
---
+++ Example.WebApi/Global.asax.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Example.WebApi
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
