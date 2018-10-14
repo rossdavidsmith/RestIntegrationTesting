9166b4b30b2d3e9b82b8821174caf310f96015b4
Ross Smith <ross.david.smith@gmail.com>
2018-10-14 14:47:22 +0100

    Add simple integration test with OWIN TestServer
    - The test:
        - creates an OWIN TestServer using the same configuration as
          the real WebApi.
        - creates an HttpClient using the `TestServer.Handler` as the
          `HttpMessageHandler`.
        - submits a request with a URL based on `TestServer.BaseAddress`.

---
+++ Example.WebApi.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Example.WebApi.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Example.WebApi.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0feab9c-4154-4745-b1d7-1fd7cb4a05d0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Example.WebApi.IntegrationTests/ValuesTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin.Testing;
+using Owin;
+using Xunit;
+
+namespace Example.WebApi.IntegrationTests
+{
+    public class ValuesTests
+    {
+        [Fact]
+        public async Task Should_ReturnValues_When_GetValuesIsCalled()
+        {
+            // [arrange]
+            using (var testServer = CreateTestServer())
+            {
+                var httpClient = new HttpClient(testServer.Handler);
+
+                var expectedValues = new[] { "value1", "value2" };
+
+                // [act]
+                var response = await httpClient.GetAsync(testServer.BaseAddress + "/" + "api/values/");
+
+                // [assert]
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+                var listResponse = await response.Content.ReadAsAsync<IReadOnlyList<string>>();
+
+                Assert.Equal(expectedValues, listResponse);
+            }
+        }
+
+        private TestServer CreateTestServer()
+        {
+            return TestServer.Create(CreateConfiguration);
+        }
+
+        private void CreateConfiguration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
